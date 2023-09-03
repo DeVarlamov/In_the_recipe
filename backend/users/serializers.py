4d5109071af0f0dcc80d53dcc4d4beaa6628e81f
Nikolay <devarlamov1@yandex.ru>
2023-09-04 02:32:12 +0300

    юзер2/1

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,6 +1,4 @@
 from api.v1.serializers import RecipeSerializer
-from django.contrib.auth.password_validation import validate_password
-from django.core import exceptions as django_exceptions
 from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
 
