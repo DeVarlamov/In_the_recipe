9e3f5db73c0658fe5d7588452cf533cca9eaaa43
Nikolay <devarlamov1@yandex.ru>
2023-09-04 01:50:11 +0300

    откат

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,7 @@
-from api.v1.serializers import RecipeSerializer
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions as django_exceptions
 from djoser.serializers import UserCreateSerializer
+from recipes.models import Recipe
 from rest_framework import serializers
 
 from .models import Subscribed, User
@@ -41,28 +43,73 @@ class UserSerializer(serializers.ModelSerializer):
         return Subscribed.objects.filter(user=user, author=obj).exists()
 
 
+class SetPasswordSerializer(serializers.Serializer):
+    """Сереализатор изменение пароля пользователя."""
+
+    current_password = serializers.CharField()
+    new_password = serializers.CharField()
+
+    def validate(self, data):
+        try:
+            validate_password(data['new_password'])
+        except django_exceptions.ValidationError as e:
+            raise serializers.ValidationError(
+                {'new_password': list(e.messages)})
+        return data
+
+    def update(self, instance, validated_data):
+        current_password = validated_data['current_password']
+        new_password = validated_data['new_password']
+        if not instance.check_password(current_password):
+            raise serializers.ValidationError(
+                {'current_password': 'Неправильный пароль.'})
+        if current_password == new_password:
+            raise serializers.ValidationError(
+                {'new_password': 'Новый пароль должен отличаться от текущего.',
+                 })
+        instance.set_password(new_password)
+        instance.save()
+        return validated_data
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Список рецептов без ингридиентов."""
+
+    image = serializers.ReadOnlyField(source='image.url')
+    name = serializers.ReadOnlyField()
+    cooking_time = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'name',
+                  'image', 'cooking_time')
+
+
 class SubscribedSerializer(serializers.ModelSerializer):
     """Список обьектов на которые подписан юзер."""
-    recipes = serializers.SerializerMethodField(method_name='get_recipes')
-    recipes_count = serializers.SerializerMethodField(
-        method_name='get_recipes_count')
+
+    email = serializers.ReadOnlyField()
+    username = serializers.ReadOnlyField()
+    is_subscribed = serializers.SerializerMethodField()
+    recipes = RecipeSerializer(many=True, read_only=True)
+    recipes_count = serializers.SerializerMethodField()
+    first_name = serializers.ReadOnlyField()
+    last_name = serializers.ReadOnlyField()
 
     class Meta:
         model = User
         fields = ('email', 'id', 'username', 'first_name', 'last_name',
                   'is_subscribed', 'recipes', 'recipes_count')
 
+    def get_is_subscribed(self, obj):
+        return (
+            self.context.get('request').user.is_authenticated
+            and Subscribed.objects.filter(user=self.context['request'].user,
+                                          author=obj).exists()
+        )
+
     def get_recipes_count(self, obj):
         return obj.recipes.count()
-
-    def get_recipes(self, object):
-        limit = int(self.context['request'].query_params.get(
-            'recipes_limit', default=0)
-        )
-        author_recipes = object.recipes.all()[:limit]
-        return RecipeSerializer(
-            author_recipes, many=True
-        ).data
 
 
 class SubscribeAuthorSerializer(SubscribedSerializer):
