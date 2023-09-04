a8fcff4c685969f15a81046f94350a54d7d8b36a
Nikolay <devarlamov1@yandex.ru>
2023-09-04 14:13:16 +0300

    fix lis8

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -17,12 +17,30 @@ from rest_framework.serializers import (
     SerializerMethodField,
     ValidationError,
 )
-from users.serializers import UserSerializer
+from users.models import User
 
 
-class UserDateSerializer(UserSerializer):
+class UserDateSerializer(ModelSerializer):
     """Сереалайзер данных юзера."""
-    pass
+
+    is_subscribed = SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ('email',
+                  'id',
+                  'username',
+                  'first_name',
+                  'last_name',
+                  'is_subscribed',
+                  )
+
+    def get_is_subscribed(self, obj):
+        user = self.context.get('request').user
+        return (
+            user.is_authenticated
+            and obj.subscribing.filter(user=user).exists()
+        )
 
 
 class TagSerializer(ModelSerializer):
