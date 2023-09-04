b970adc10e95ce632d5c5c2ffdb93656b61d4bcb
Nikolay <devarlamov1@yandex.ru>
2023-09-04 03:23:42 +0300

    откат юзера

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,4 +1,6 @@
 from api.v1.serializers import RecipeSerializer
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions as django_exceptions
 from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
 
@@ -17,6 +19,34 @@ class UserRegistrationSerializer(UserCreateSerializer):
                   'last_name',
                   'password',
                   )
+
+
+class SetPasswordSerializer(serializers.Serializer):
+    """Сереализатор изменение пароля пользователя."""
+    current_password = serializers.CharField()
+    new_password = serializers.CharField()
+
+    def validate(self, data):
+        try:
+            validate_password(data['new_password'])
+        except django_exceptions.ValidationError as e:
+            raise serializers.ValidationError(
+                {'new_password': list(e.messages)})
+        return data
+
+    def update(self, instance, validated_data):
+        current_password = validated_data['current_password']
+        new_password = validated_data['new_password']
+        if not instance.check_password(current_password):
+            raise serializers.ValidationError(
+                {'current_password': 'Неправильный пароль.'})
+        if current_password == new_password:
+            raise serializers.ValidationError(
+                {'new_password': 'Новый пароль должен отличаться от текущего.',
+                 })
+        instance.set_password(new_password)
+        instance.save()
+        return validated_data
 
 
 class UserSerializer(serializers.ModelSerializer):
