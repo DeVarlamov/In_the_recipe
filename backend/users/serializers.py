45f58a6ee29b712e17e4812699525c84f42763bc
Nikolay <devarlamov1@yandex.ru>
2023-09-04 12:20:04 +0300

    fix lis4

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -65,10 +65,11 @@ class UserSerializer(serializers.ModelSerializer):
                   )
 
     def get_is_subscribed(self, obj):
-        user = self.context['request'].user
-        if user.is_anonymous:
-            return False
-        return Subscribed.objects.filter(user=user, author=obj).exists()
+        user = self.context.get('request').user
+        return (
+            user.is_authenticated
+            and obj.subscribing.filter(user=user).exists()
+        )
 
 
 class SubscribedSerializer(serializers.ModelSerializer):
